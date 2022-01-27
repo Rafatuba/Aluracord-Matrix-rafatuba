e0b73753f3867380acf33d0919779f4bbf96fa7b
Rafatuba <rafaolly1@gmail.com>
2022-01-27 10:07:22 -0400

    atualização Aula2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
+import React from "react";
+import {useRouter} from 'next/router';
 
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-            * {
-                margin: 0;
-                padding: 0;
-                box-sizing: border-box;
-                list-style: none;
-            }
-            body {
-                font-family: 'Open Sans', sans-serif;
-            }
-            /*App fit Height */
-            html, body, #__next {
-                min-height: 100vh;
-                display: flex;
-                flex: 1;
-            }
-            #__next {
-                flex: 1;
-            }
-            #__next > * {
-                flex: 1;
-            }
-            /* ./App fit Height*/
-        `}</style>
-    );
-}
+
 
 
 function Titulo(props) {
@@ -62,16 +37,19 @@ function HomePage() {
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    //const username = 'Rafatuba';
+    const [username, setUsername] = React.useState('Rafatuba');
+    //console.log('stateDoReact', stateDoReact);
+    const roteamento = useRouter();
+    //console.log(roteamento);
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
-                    backgroundColor: appConfig.theme.colors.primary[500],
-                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+                    backgroundColor: appConfig.theme.colors.primary[100],
+                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/12/bright-gaming-room-setup-1536x864.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
             >
@@ -87,23 +65,38 @@ export default function PaginaInicial() {
                         width: '100%', maxWidth: '700px',
                         borderRadius: '5px', padding: '32px', margin: '16px',
                         boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                        backgroundColor: "rgba(33, 41, 48, 0.80)",
                     }}
                 >
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form');
+                            //window.location.href = '/chat';
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Titulo tag="h2">Boas vindas de volta!</Titulo>
+                        <Titulo tag="h2">Bem Vindo</Titulo>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
 
+                            
                         <TextField
+                            value={username}
+                            onChange={function(event) {
+                                console.log('está digitando', event.target.value);
+                                // Onde está o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variável através do React
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
@@ -116,7 +109,7 @@ export default function PaginaInicial() {
                         />
                         <Button
                             type='submit'
-                            label='Entrar'
+                            label='Clique aqui para Entrar'
                             fullWidth
                             buttonColors={{
                                 contrastColor: appConfig.theme.colors.neutrals["000"],
@@ -147,7 +140,7 @@ export default function PaginaInicial() {
                     >
                         <Image
                             styleSheet={{
-                                borderRadius: '50%',
+                                borderRadius: '10%',
                                 marginBottom: '16px',
                             }}
                             src={`https://github.com/${username}.png`}
